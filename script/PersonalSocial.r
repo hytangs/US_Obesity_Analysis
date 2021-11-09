cf0fb35a4456923a9b82658751378451b8690ed0
hytangs <64950573+hytangs@users.noreply.github.com>
2021-11-10 03:28:35 +0800

    Update PersonalSocial.r

--- script/PersonalSocial.r
+++ script/PersonalSocial.r
@@ -14,7 +14,7 @@ plot_usmap(data = CHR21_county, regions = c("counties"), values = "ObesityPercen
   scale_fill_continuous(low = "mistyrose", high = "blue", name = "Obesity Percent", label = scales::comma) + 
   theme(legend.position = "right")
 
-# Normalization of Non- Rate / Percent Factors
+# Normalization of Non-Rate / Non-Percent Factors
 summary(CHR21_county$MedianIncome)
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
 #  24732   46243   53341   55713   62059  151806       1 
@@ -22,13 +22,33 @@ CHR21_county$MedianIncomeNormalized = (CHR21_county$MedianIncome - 24732) / 5571
 
 # A. Population Demographics
 # A1. Age Range
-
+plot_usmap(data = CHR21_county, regions = c("counties"), values = "LifeExpectancy", color = "darkred", label_color = "white") + 
+  scale_fill_continuous(low = "mistyrose", high = "blue", name = "Expected Ages", label = scales::comma) + 
+  theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
+  labs(title = "Life Expectancy", subtitle = "US County Level Life Expectancy Choropleth Map") 
 
 # A2. Gender
+plot_usmap(data = CHR21_county, regions = c("counties"), values = "FemalePercent", color = "darkred", label_color = "white") + 
+  scale_fill_continuous(low = "mistyrose", high = "blue", name = "Percentage", label = scales::comma) + 
+  theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
+  labs(title = "Female Percentage", subtitle = "US County Level Female Percentage Choropleth Map") 
 
+# Gender dominance zonal map
+
+# Summary of Gender Levels to Obesity
 
 # A3. Race
 
+# Race Choropleth Map
+
+
+# Operation: Zoning by Dominant Minority Rate
+
+
+# Operation: Get Zonal Value
+
+
+# Operation: 
 # A4. Migration Flow
 
 
