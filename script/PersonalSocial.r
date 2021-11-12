3068cf46146788bb00d9d8dc34bf84cf4938704a
hytangs <64950573+hytangs@users.noreply.github.com>
2021-11-12 21:33:29 +0800

    Update

--- script/PersonalSocial.r
+++ script/PersonalSocial.r
@@ -11,6 +11,9 @@ library(ggplot2)
 library(ggpubr)
 library(readxl)
 library(dplyr)
+library(spatialreg)
+library(spdep)
+library(sf)
 
 
 # Dataset Load
@@ -22,6 +25,22 @@ head(CHR21_county)
 head(CHR21_state)
 CHR21_county <- as.data.frame(CHR21_county)
 CHR21_state <- as.data.frame(CHR21_state)
+
+
+# Import Spatially Processed CHR21 Dataset 
+CHR21_sf <- read_excel("CountryHealthRankings21_Cleaned.xlsx", 
+                          sheet = "sf")
+
+require(rgdal)
+shape <- readOGR(dsn = ".", layer = "cb_2018_us_county_20m")
+
+require(sf)
+shape <- read_sf(dsn = ".", layer = "cb_2018_us_county_20m")
+
+CHR21_sf <- merge(CHR21_sf, shape, by.x="fips", by.y="GEOID")
+CHR21_sf.sf <- st_as_sf(CHR21_sf)
+state.nb <- poly2nb(CHR21_sf.sf)
+state.lw <- nb2listw(state.nb, zero.policy = TRUE)
 
 
 # County Level Obesity Percent Map
@@ -45,15 +64,28 @@ summary(CHR21_state$MedianIncome)
 #  45928   57503   63290   65616   75117   90395 
 CHR21_state$MedianIncomeNormalized = (CHR21_state$MedianIncome - 45928) / 63290 * 50
 
+summary(CHR21_sf$MedianIncome)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 24732   46249   53362   55702   62038  151806    1 
+CHR21_sf$MedianIncomeNormalized = (CHR21_sf$MedianIncome - 24732) / 53362 * 50
+
 
 # A. Population Demographics
 
 
 # A1. Age Range
+
+
 # Life Expectancy
+# Regular Regression
 lm_life_fit <- lm(ObesityPercent ~ LifeExpectancy, data = CHR21_county)
 summary(lm_life_fit)
 
+# Spatial Autoregression
+sar.res_life <-spautolm(ObesityPercent ~ LifeExpectancy, listw = state.lw, data = CHR21_sf.sf, zero.policy = TRUE)
+summary(sar.res_life)
+
+# Plot
 plot_usmap(data = CHR21_county, regions = c("counties"), values = "LifeExpectancy", color = "darkred", label_color = "white") + 
   scale_fill_continuous(low = "mistyrose", high = "blue", name = "Expected Ages", label = scales::comma) + 
   theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
@@ -69,6 +101,10 @@ plot_usmap(data = CHR21_state, values = "LifeExpectancy", color = "darkred", lab
 lm_child_fit <- lm(ObesityPercent ~ ChildPercent, data = CHR21_county)
 summary(lm_child_fit)
 
+sar.res_child <-spautolm(ObesityPercent ~ ChildPercent, listw = state.lw, data = CHR21_sf.sf, zero.policy = TRUE)
+summary(sar.res_child)
+
+# Plot
 plot_usmap(data = CHR21_county, values = "ChildPercent", color = "darkred", label_color = "white") + 
   scale_fill_continuous(low = "mistyrose", high = "blue", name = "Child Percentage", label = scales::comma) + 
   theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
@@ -84,6 +120,9 @@ plot_usmap(data = CHR21_state, values = "ChildPercent", color = "darkred", label
 lm_elder_fit <- lm(ObesityPercent ~ ElderlyPercent, data = CHR21_county)
 summary(lm_elder_fit)
 
+sar.res_elderly <-spautolm(ObesityPercent ~ ChildPercent, listw = state.lw, data = CHR21_sf.sf, zero.policy = TRUE)
+summary(sar.res_elderly)
+
 plot_usmap(data = CHR21_county, values = "ElderlyPercent", color = "darkred", label_color = "white") + 
   scale_fill_continuous(low = "mistyrose", high = "blue", name = "Elderly Percentage", label = scales::comma) + 
   theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
@@ -96,7 +135,7 @@ plot_usmap(data = CHR21_state, values = "ElderlyPercent", color = "darkred", lab
 
 
 # A2. Gender
-plot_usmap(data = CHR21_county, regions = c("counties"), values = "FemalePercent", color = "darkred", label_color = "white") + 
+plot_usmap(data = CHR21_county, regions = c("counties"), values = "FemalePercent", color = "white", label_color = "white") + 
   scale_fill_continuous(low = "mistyrose", high = "blue", name = "Percentage", label = scales::comma) + 
   theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
   labs(title = "Female Percentage", subtitle = "US County Level Female Percentage Choropleth Map") 
@@ -164,14 +203,41 @@ summary(lm_gender_fit)
 
 # By Percentage of White Majority Choropleth Map
 plot_usmap(data = CHR21_county, regions = c("counties"), values = "WhitePercent", color = "white", label_color = "white") + 
-  scale_fill_continuous(low = "mistyrose", high = "blue", name = "Percentage", label = scales::comma) + 
+  scale_fill_continuous(low = "lightyellow", high = "darkred", name = "Percentage", label = scales::comma) + 
   theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
   labs(title = "White Percentage", subtitle = "US County Level White Majority Percentage Choropleth Map") 
 
 plot_usmap(data = CHR21_state, values = "WhitePercent", color = "white", label_color = "white") + 
-  scale_fill_continuous(low = "mistyrose", high = "blue", name = "Percentage", label = scales::comma) + 
+  scale_fill_continuous(low = "lightyellow", high = "darkred", name = "Percentage", label = scales::comma) + 
   theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
   labs(title = "White Percentage", subtitle = "US State Level White Majority Percentage Choropleth Map") 
+
+# Plot Minority Race Percentage
+plot_usmap(data = CHR21_county, regions = c("counties"), values = "BlackPercent", color = "white", label_color = "white") + 
+  scale_fill_continuous(low = "lightyellow", high = "brown", name = "Percentage", label = scales::comma) + 
+  theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
+  labs(title = "Black Percentage", subtitle = "US County Level Black People Percentage Choropleth Map") 
+
+plot_usmap(data = CHR21_county, regions = c("counties"), values = "NativeAmericanPercent", color = "white", label_color = "white") + 
+  scale_fill_continuous(low = "lightyellow", high = "blue", name = "Percentage", label = scales::comma) + 
+  theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
+  labs(title = "Native American Percentage", subtitle = "US County Level Native American Percentage Choropleth Map") 
+
+plot_usmap(data = CHR21_county, regions = c("counties"), values = "AsianPercent", color = "white", label_color = "white") + 
+  scale_fill_continuous(low = "lightyellow", high = "pink", name = "Percentage", label = scales::comma) + 
+  theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
+  labs(title = "Asian Percentage", subtitle = "US County Level Asian People Percentage Choropleth Map") 
+
+plot_usmap(data = CHR21_county, regions = c("counties"), values = "IslanderPercent", color = "white", label_color = "white") + 
+  scale_fill_continuous(low = "lightyellow", high = "black", name = "Percentage", label = scales::comma) + 
+  theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
+  labs(title = "Islander Percentage", subtitle = "US County Level Pacific Islander People Percentage Choropleth Map") 
+
+plot_usmap(data = CHR21_county, regions = c("counties"), values = "HispanicPercent", color = "white", label_color = "white") + 
+  scale_fill_continuous(low = "lightyellow", high = "brown", name = "Percentage", label = scales::comma) + 
+  theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
+  labs(title = "Hispanic Percentage", subtitle = "US County Level Hispanic People Percentage Choropleth Map") 
+
 
 # Dominant Minority Race Function
 getMinority <- function(BlackPercent, NativeAmericanPercent, AsianPercent, IslanderPercent, HispanicPercent)
@@ -233,7 +299,25 @@ CHR21_state %>%
 
 
 # B. Social Economic Influential Factors
+sar.res_first_round <-spautolm(ObesityPercent ~ AdultSmokingPercent + DrinkingPercent +
+                          FoodEnvironmentIndex + PhysicalInactiveRate + ExerciseAccessPercent + 
+                          SexualDiseaseRate + TeenBirthRate + UninsuredPercent + 
+                          PrimaryCarePhysiciansRate + DentistRate + HighSchoolCompletionRate +
+                          UnemployPercent + MedianIncomeNormalized + IncomeInequilityRatio +
+                          ChildrenPovertyPercent + HousingProblemPercent + ViolentCrimeRate +
+                          PoorHealthPercent + FoodInsecurePercent +
+                          LimitedHealthyFoodAccessPercent + InsufficientSleepPercent, 
+                        data = CHR21_sf.sf, listw = state.lw, zero.policy = TRUE)
+summary(sar.res_first_round)
 
+sar1.res_second_round <- spautolm(ObesityPercent ~ AdultSmokingPercent + 
+                                    FoodEnvironmentIndex + PhysicalInactiveRate + 
+                                    UninsuredPercent + UnemployPercent + IncomeInequilityRatio +
+                                    ChildrenPovertyPercent + HousingProblemPercent +
+                                    PoorHealthPercent + FoodInsecurePercent +
+                                    LimitedHealthyFoodAccessPercent + InsufficientSleepPercent, 
+                                  data = CHR21_sf.sf, listw = state.lw, zero.policy = TRUE)
+summary(sar1.res_second_round)
 # B1. Feature Identification and Selection
 # Simple Linear Regression For Feature Selection
 lm_model <- lm(ObesityPercent ~ AdultSmokingPercent + DrinkingPercent + DrinkingPercent +
@@ -256,6 +340,9 @@ lm_model_second_round <- lm(ObesityPercent ~ FoodEnvironmentIndex + PhysicalInac
 summary(lm_model_second_round)
 
 
+# Spatial Regression:
+CHR21_sf.sf <- st_as_sf(CHR21_sf)
+
 # Hypothesis 1: Regions with poor Physical Health 
 # PhysicalInactiveRate, InsufficientSleepPercent, PoorHealthPercent
 # EDA
@@ -265,17 +352,23 @@ ggscatter(CHR21_county, y = "ObesityPercent", x = "PhysicalInactiveRate",
           title = "Inspection on Physical Inactive Rate and Obesity of all counties",
           ylab = "Obesity % of Population", xlab = "Physical Inactive Rate")
 
-ggscatter(CHR21_county, y = "ObesityPercent", x = "InsufficientSleepPercent", 
+ggscatter(CHR21_county, y = "ObesityPercent", x = "AdultSmokingPercent", 
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
-          title = "Inspection on Insufficient Sleep and Obesity of all counties",
-          ylab = "Obesity % of Population", xlab = "Insufficient Sleep %")
+          title = "Inspection on Adult Smoke Rate and Obesity of all counties",
+          ylab = "Obesity % of Population", xlab = "Adult Smoke %")
 
 ggscatter(CHR21_county, y = "ObesityPercent", x = "PoorHealthPercent", 
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
           title = "Inspection on Poor Health Percent and Obesity of all counties",
           ylab = "Obesity % of Population", xlab = "Poor Health %")
+
+ggscatter(CHR21_county, y = "ObesityPercent", x = "InsufficientSleepPercent", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          title = "Inspection on Insufficient Sleep and Obesity of all counties",
+          ylab = "Obesity % of Population", xlab = "Insufficient Sleep %")
 
 
 # Spatial Analysis on Health Factors
@@ -284,6 +377,10 @@ plot_usmap(data = CHR21_county, regions = c("counties"), values = "PhysicalInact
   theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
   labs(title = "White Percentage", subtitle = "US County Level Physical Inactive Rate Choropleth Map") 
 
+plot_usmap(data = CHR21_state, values = "PhysicalInactiveRate", color = "white", label_color = "white") + 
+  scale_fill_continuous(low = "lightgreen", high = "darkblue", name = "Physical Inactive Rate", label = scales::comma) + 
+  theme(panel.background = element_rect(colour = "black", fill = "lightblue"), legend.position = "right") +
+  labs(title = "White Percentage", subtitle = "US State Level Physical Inactive Rate Choropleth Map") 
 
 
 # Hypothesis 2: Food Related Factor: Regions with lower food environment 
